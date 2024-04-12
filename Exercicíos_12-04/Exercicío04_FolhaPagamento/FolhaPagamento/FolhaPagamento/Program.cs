44ddb6c2b721cdd46bac54fba5593af6f7f7c96e
GuiNakamuraC <124791730+GuiNakamuraC@users.noreply.github.com>
2024-04-12 11:42:22 -0300

    Add files via upload

---
+++ Exercicíos_12-04/Exercicío04_FolhaPagamento/FolhaPagamento/FolhaPagamento/Program.cs
@@ -0,0 +1,22 @@
+﻿using FolhaPagamento;
+
+namespace FolhaPagamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pagamento p = new Pagamento();
+            p.Nome = "Ana";
+            p.Sal = 5000;
+            p.HED = 0;
+            p.HEN = 12;
+            p.ND = 0;
+            p.Fal = 20;
+            p.DE = 5;
+            p.REF = 5;
+            p.Val = 1;
+            Console.WriteLine(p.FolhaDePagamento());
+        }
+    }
+}
